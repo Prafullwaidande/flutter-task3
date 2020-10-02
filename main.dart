a25bb5e51459d9180e64bf18b34942102b3a7a9d
PrafullWaidande <64540814+Prafullwaidande@users.noreply.github.com>
2020-10-02 22:49:25 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:justfirst/myanimation.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Animation'),
+        ),
+        body: MyA1(),
+      ),
+    ),
+  );
+}
